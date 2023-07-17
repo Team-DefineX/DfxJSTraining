053a8b07c96c40c1d0b7faa3f0b7388449f7d9d0
avcularozan <ozan.avcular.oa@gmail.com>
2023-07-17 22:16:35 +0300

    added recursive functions step

--- resources/moduleData.js
+++ resources/moduleData.js
@@ -95,7 +95,7 @@ export const modules = [
         name: "Accessing Properties",
         time: "5mins",
       },
-      { 
+      {
         name: "Bracket Notation",
         time: "5mins",
       },
@@ -155,6 +155,10 @@ export const modules = [
       },
       {
         name: "Callback functions",
+        time: "16mins",
+      },
+      {
+        name: "Recursive functions",
         time: "16mins",
       },
     ],
